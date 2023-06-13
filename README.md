e6224baff80de330662e743dec96ed5b4717b518
pier <pierknows@gmail.com>
2023-06-13 17:22:13 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Describe: BankAccount ();
+
+Test: "It should create the initial bank account entry"
+Input: BankAccount (250);
+Expected Output: BankAccount (250);
+
+Test:
+Input:
+Expected Output: 
---
+++ js/scripts.js
@@ -0,0 +1,63 @@
+// Business Logic
+
+function BankAccount() {
+this.initialEntry = 0;
+this.currentId = 0;
+this.withdrawals = 0;
+this.deposits = 0;
+
+};
+
+BankAccount.prototype.addEntryDeposit = function(entry) {
+    entry.id = this.assignId();
+    this.initialEntry[entry.id] = entry;
+};
+
+BankAccount.prototype.assignId = function() {
+this.currentId += 1;
+    return this.currentId;
+};
+
+BankAccount.prototype.findEntry = function(id) {
+    if (this.initialEntry[id] !== undefined) {
+        return this.initialEntry[id];
+    }
+    return false;
+};
+
+//WITHDRAWAL
+BankAccount.prototype.removeWithdrawal = function(withdrawal) {
+    withdrawal.id = this.assignId();
+    this.withdrawals[withdrawal.id] = withdrawal;
+};
+
+BankAccount.prototype.assignId = function() {
+this.currentId += 1;
+    return this.currentId;
+};
+
+BankAccount.prototype.findWithdrawal = function(id) {
+    if (this.withdrawals[id] !== undefined) {
+        return this.withdrawals[id];
+    }
+    return false;
+};
+
+//DEPOSIT
+BankAccount.prototype.addDeposit = function(deposit) {
+  deposit.id = this.assignId();
+  this.Deposit[deposit.id] = deposit;
+};
+
+BankAccount.prototype.assignId = function() {
+this.currentId += 1;
+  return this.currentId;
+};
+
+BankAccount.prototype.findDeposit = function(id) {
+  if (this.deposits[id] !== undefined) {
+      return this.deposits[id];
+  }
+  return false;
+};
+
